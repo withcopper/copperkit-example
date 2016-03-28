9b2c5b0561c7aec572216ded34017c00f3d5fcdf
Doug Williams <dougw@igudo.com>
2016-03-27 18:59:18 -0700

    Ensure signout button's placement is always tapable

--- CopperKitTest/ViewController.swift
+++ CopperKitTest/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.signedInView.setNeedsLayout()
         // reset our UI elements to the Signed Out state
         resetView()
     }
